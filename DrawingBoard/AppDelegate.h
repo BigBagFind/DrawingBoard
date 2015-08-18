5f39879eb81f453436507ea719f51b67d38fc0ce
吴玉铁 <Ladn@dingjunxiaodeMacBook-Pro.local>
2015-08-18 17:42:38 +0800

    Initial Commit

---
+++ DrawingBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingBoard
+//
+//  Created by 吴玉铁 on 15/8/18.
+//  Copyright (c) 2015年 铁哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
